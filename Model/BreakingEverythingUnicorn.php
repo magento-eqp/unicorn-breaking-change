9af1a6f4cba1e1d48bcddb6260f7f68c08725140
Lena Orobei <oorobei@magento.com>
2019-04-11 14:15:03 -0500

    Hackathon: breaking change

---
+++ Model/BreakingEverythingUnicorn.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Unicorn\MagicUpdateDemo\Model;
+
+/**
+ * Just a simple Unicorn that breaks everything.
+ */
+class BreakingEverythingUnicorn
+{
+
+}
